849fc4ec50c299a2805c1ce1b76037b63f9baf09
Jorge G <jorgegomezweb@gmail.com>
2021-08-24 12:51:45 +0200

    FIX: separating logic in functions

--- js/app.js
+++ js/app.js
@@ -3,28 +3,33 @@ const displayImg = document.querySelector('.display-image');
 const imgLink = document.querySelector('.image-link');
 let uploadedImage = '';
 
-imageInput.addEventListener('change', function () {
-	const reader = new FileReader();
-
-	const file = imageInput.files[0];
-	const url = URL.createObjectURL(file);
-
-	reader.addEventListener('load', () => {
-		uploadedImage = reader.result;
-
-		displayImg.style.backgroundImage = `url(${uploadedImage})`;
-	});
-
-	reader.readAsDataURL(this.files[0]);
-
-	downloadImage(url, this.files[0]);
-});
-
 const downloadImage = async (imgSrc, { name }) => {
 	const image = await fetch(imgSrc);
 	const imageBlob = await image.blob();
 	const imageURL = URL.createObjectURL(imageBlob);
 
+	imgLink.classList.remove('is-disabled');
 	imgLink.href = imageURL;
 	imgLink.download = name || 'download';
 };
+
+const createImage = (file) => {
+	const reader = new FileReader();
+
+	reader.addEventListener('load', () => {
+		uploadedImage = reader.result;
+		displayImg.style.opacity = '0.5';
+		displayImg.style.backgroundImage = `url(${uploadedImage})`;
+	});
+
+	reader.readAsDataURL(file);
+};
+
+imageInput.addEventListener('change', function () {
+	const file = this.files[0];
+	const url = URL.createObjectURL(file);
+
+	createImage(file);
+
+	downloadImage(url, file);
+});
