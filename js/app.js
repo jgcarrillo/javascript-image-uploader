d655dd9b8d133cfddb5741d894af86fb75a3fb07
Jorge G <jorgegomezweb@gmail.com>
2021-08-24 17:54:50 +0200

    FIX: Adding new procedure to draw and download image from canvas

--- js/app.js
+++ js/app.js
@@ -8,50 +8,77 @@ const downloadImage = async (imgSrc, { name }) => {
 	const imageBlob = await image.blob();
 	const imageURL = URL.createObjectURL(imageBlob);
 
-	imgLink.classList.remove('is-disabled');
-	imgLink.href = imageURL;
-	imgLink.download = 'downloadedImage';
+	createLink(imgLink, imageURL);
 };
 
-const createImage = (file) => {
+const createLink = (link, url) => {
+	link.classList.remove('is-disabled');
+	link.href = url;
+	link.download = 'downloadedImage';
+};
+
+const createImageElement = (imageURL) => {
+	const $image = document.createElement('img');
+
+	$image.src = imageURL;
+	$image.classList.add('canvas__img');
+
+	return $image;
+};
+
+const displayImage = (file) => {
 	const reader = new FileReader();
 
 	reader.addEventListener('load', () => {
-		localStorage.setItem('uploaded-image', reader.result);
-
 		uploadedImage = reader.result;
 
-		// displayImg.style.backgroundImage = `url(${uploadedImage})`;
-
-		getLocalStorageImage();
+		displayImg.style.backgroundImage = `url(${uploadedImage})`;
 	});
 
 	reader.readAsDataURL(file);
 };
 
-const getLocalStorageImage = () => {
-	const getImage = localStorage.getItem('uploaded-image');
+const getImg = async (url, imgElem) => {
+	const ctx = displayImg.getContext('2d');
 
-	if (getImage) {
-		displayImg.style.backgroundImage = `url(${getImage})`;
-	} else {
-		displayImg.style.backgroundColor = '';
-	}
+	const res = await fetch(url);
+	const b = await res.blob();
+	const imageUrl = URL.createObjectURL(b);
+	console.log(imageUrl);
+
+	imgElem.addEventListener('load', () => {
+		URL.revokeObjectURL(imageUrl);
+	});
+	ctx.drawImage(imgElem, 0, 0);
+	imgElem.src = imageUrl;
 };
 
-imageInput.addEventListener('change', function () {
-	const file = this.files[0];
+imageInput.addEventListener('change', (e) => {
+	const file = e.target.files[0];
 	const url = URL.createObjectURL(file);
+	const ctx = displayImg.getContext('2d');
+
+	imageElem = createImageElement(url);
+	// getImg(url, imageElem);
+
+	imageElem.addEventListener('load', () => {
+		ctx.drawImage(imageElem, displayImg.width / 2 - imageElem.width / 2, displayImg.height / 2 - imageElem.height / 2);
+
+		const link = document.createElement('a');
+		link.download = 'canvas';
+		link.href = displayImg.toDataURL();
+		console.log(link);
+		link.click();
+	});
+
+	displayImg.width = 1000;
+	displayImg.height = 1000;
+	// ctx.fillStyle = 'red';
+	// ctx.fillRect(0, 0, displayImg.width, displayImg.height);
 
 	// CREATE PREVIEW IMAGE
-	createImage(file);
+	// displayImage(file);
 
 	// DOWNLOAD THE PREVIOUS UPLOADED IMAGE
-	downloadImage(url, file);
-
-	getLocalStorageImage();
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-	getLocalStorageImage();
+	// downloadImage(url, file);
 });
