147fc016f5f29310195bc2fc72bd75f305b0eafb
Jorge G <jorgegomezweb@gmail.com>
2021-08-25 09:40:18 +0200

    FIX: fixing canvas and stroke size

--- js/app.js
+++ js/app.js
@@ -2,54 +2,6 @@ const input = document.querySelector('.btn-input');
 const canvas = document.querySelector('.canvas');
 const link = document.querySelector('.link');
 
-/*
-const downloadImage = async (imgSrc, { name }) => {
-	const image = await fetch(imgSrc);
-	const imageBlob = await image.blob();
-	const imageURL = URL.createObjectURL(imageBlob);
-
-	createLink(link, imageURL);
-};
-*/
-
-/*
-const createLink = (link, url) => {
-	link.classList.remove('link--disabled');
-	link.href = url;
-	link.download = 'downloadedImage';
-};
-*/
-
-/*
-const displayImage = (file) => {
-	const reader = new FileReader();
-
-	reader.addEventListener('load', () => {
-		uploadedImage = reader.result;
-
-		canvas.style.backgroundImage = `url(${uploadedImage})`;
-	});
-
-	reader.readAsDataURL(file);
-};
-*/
-
-/*
-const getImg = async (url, imgElem) => {
-	const ctx = canvas.getContext('2d');
-
-	const res = await fetch(url);
-	const b = await res.blob();
-	const imageUrl = URL.createObjectURL(b);
-
-	imgElem.addEventListener('load', () => {
-		URL.revokeObjectURL(imageUrl);
-	});
-	ctx.drawImage(imgElem, 0, 0);
-	imgElem.src = imageUrl;
-};
-*/
-
 const createImageElement = (imageURL) => {
 	const $image = document.createElement('img');
 
@@ -77,27 +29,15 @@ const setBlur = (ctx, colour, size) => {
 	ctx.shadowBlur = size;
 };
 
-const setStroke = (ctx) => {
+const setStroke = (ctx, px, py, imX, imY) => {
 	ctx.strokeStyle = 'white';
 	ctx.lineJoin = 'round';
 	ctx.lineWidth = 6;
-	ctx.strokeRect(300, 200, 800, 500);
+	ctx.strokeRect(px, py, imX, imY);
 };
 
-const drawImage = (ctx, image, canvas) => {
-	ctx.drawImage(
-		image,
-		0,
-		0,
-		image.width,
-		image.height,
-		// canvas.width / 2 - imageElem.width / 2,
-		// canvas.height / 2 - imageElem.height / 2,
-		300,
-		200,
-		800,
-		500
-	);
+const drawImage = (ctx, image, px, py, imX, imY) => {
+	ctx.drawImage(image, 0, 0, image.width, image.height, px, py, imX, imY);
 };
 
 input.addEventListener('change', (e) => {
@@ -113,6 +53,18 @@ input.addEventListener('change', (e) => {
 	canvas.width = 1500;
 	canvas.height = 900;
 
+	/**********************
+	SET HERE THE IMAGE SIZE
+	**********************/
+	const imX = 1050;
+	const imY = 750;
+
+	/*******************************************
+	SET HERE THE POSITION OF THE IMAGE IN CANVAS
+	*******************************************/
+	const px = 230;
+	const py = 80;
+
 	/******************
 	BACKGROUND GRADIENT
 	*******************/
@@ -122,7 +74,7 @@ input.addEventListener('change', (e) => {
 		/*******************
 		DRAW IMAGE ON CANVAS
 		*******************/
-		drawImage(ctx, imageElem, canvas);
+		drawImage(ctx, imageElem, px, py, imX, imY);
 
 		/*********
 		IMAGE BLUR
@@ -132,7 +84,7 @@ input.addEventListener('change', (e) => {
 		/****************
 		IMAGE STROLE PATH
 		****************/
-		setStroke(ctx);
+		setStroke(ctx, px, py, imX, imY);
 
 		/*********************************
 		CREATE AND ENABLED DOWNLOAD BUTTON
